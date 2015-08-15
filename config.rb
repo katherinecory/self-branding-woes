38e60faf65622c73e5b58bc4cd55117f4385ba85
Katherine Cory <hello@katherinecory.com>
2015-08-15 16:49:21 +0100

    Add compass config.rb.

---
+++ config.rb
@@ -0,0 +1,11 @@
+# Location of the theme's resources.
+css_dir = "css"
+sass_dir = "sass"
+images_dir = "images"
+javascripts_dir = "js"
+
+# Require any additional compass plugins installed on your system.
+require 'compass-normalize'
+require 'toolkit'
+require 'breakpoint'
+require 'singularitygs'
